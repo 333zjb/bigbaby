25e09fd187c4d193941709c68f68715e5afc0596
zhangjb <zhangjb@hzanchu.com>
2017-10-19 15:37:37 +0800

    test phpunit

--- application/tests/first_test.php
+++ application/tests/first_test.php
@@ -8,8 +8,31 @@
 
 class First_test extends \PHPUnit\Framework\TestCase
 {
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct(null, [
+            [1,1,2],
+            [2,1,2]
+        ]);
+    }
+
+    public function testdata(){
+        $a = $this->getProvidedData();
+        foreach ($a as $v){
+            $this->assertEquals($v[2], $v[0]+$v[1]);
+        }
+
+    }
     public function testModel(){
         $a = new Base_model();
         $this->assertTrue(is_object($a)==true);
     }
+    public function testResque(){
+        $b = new Resque_service();
+        $args = ['name'=>'kkk', 'age'=>16];
+        $r = $b->create_job($args, 'Fk_job');
+        $this->assertNotEmpty($r);
+    }
+
+
 }
