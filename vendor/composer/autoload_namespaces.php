b3afcefa02c19a98a8694f4cf69b69e03b25dbce
zhangjb <zhangjb@hzanchu.com>
2017-10-19 09:43:48 +0800

    操，添加个phpunit，给我添加了这么多东西

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,5 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Resque' => array($vendorDir . '/chrisboulton/php-resque/lib'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
 );
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,10 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/type-resolver/src', $vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'Predis\\' => array($vendorDir . '/predis/predis/src'),
     'Medoo\\' => array($vendorDir . '/catfan/medoo/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
