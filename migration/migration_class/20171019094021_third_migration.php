e1b2b1f47c5086e65e29d7940433cd81eff5948e
zhangjb <zhangjb@hzanchu.com>
2017-10-20 10:24:22 +0800

    phinx的一些使用尝试，和未解决问题

---
+++ migration/migration_class/20171019094021_third_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class ThirdMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        //创建一张表
+        $table = $this->table('haha');
+        $table->addColumn('user_id', 'integer')
+            ->addColumn('created_at', 'datetime')
+            ->create();
+
+    }
+}
